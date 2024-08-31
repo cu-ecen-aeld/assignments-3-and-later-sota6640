6f973a48c6b497dafce2a31e228770d98ace099b
stamrakar <stamrakar@Ubuntu.myguest.virtualbox.org>
2024-08-31 15:36:24 -0600

    unit-test should pass on self and github runner

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,5 +18,7 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    const char *hard_coded = my_username();
+    const char *conf_read = malloc_username_from_conf_file();
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(hard_coded, conf_read, "ST: Assertion Failed");
 }
